3e0558ee2d0c0ca74add51b98e874765ea5257b6
tamasstephen <kicsitnagyon@gmail.com>
2022-06-16 19:54:40 +0200

    chore: Add input label

--- component/Input.tsx
+++ component/Input.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styles from "../styles/components/Input.module.css";
 
 interface Props {
   value: string;
   setValue: Function;
+  name: string;
+  label: string;
 }
 
-const Input = ({ value, setValue }: Props) => {
+const Input = ({ value, setValue, name, label }: Props) => {
   return (
-    <input type="text" value={value} onChange={(event) => setValue(event)} />
+    <div>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        placeholder={label}
+        type="text"
+        value={value}
+        name={name}
+        id={name}
+        onChange={(event) => setValue(event)}
+      />
+    </div>
   );
 };
 
