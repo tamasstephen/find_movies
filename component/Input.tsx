6ce663a2c1f489a1b8ea262787dcaea88a7cedcc
tamasstephen <kicsitnagyon@gmail.com>
2022-06-16 19:56:44 +0200

    Revert "chore: Add input label"
    This reverts commit 3e0558ee2d0c0ca74add51b98e874765ea5257b6.

--- component/Input.tsx
+++ component/Input.tsx
@@ -1,28 +1,13 @@
 import React from "react";
-import styles from "../styles/components/Input.module.css";
 
 interface Props {
   value: string;
   setValue: Function;
-  name: string;
-  label: string;
 }
 
-const Input = ({ value, setValue, name, label }: Props) => {
+const Input = ({ value, setValue }: Props) => {
   return (
-    <div>
-      <label htmlFor={name} className={styles.label}>
-        {label}
-      </label>
-      <input
-        placeholder={label}
-        type="text"
-        value={value}
-        name={name}
-        id={name}
-        onChange={(event) => setValue(event)}
-      />
-    </div>
+    <input type="text" value={value} onChange={(event) => setValue(event)} />
   );
 };
 
