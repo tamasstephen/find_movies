3f435848e08cf18abae6b4629389218808286c84
tamasstephen <kicsitnagyon@gmail.com>
2022-06-19 14:10:37 +0200

    test: Show detail page after click

--- __test__/Index.test.tsx
+++ __test__/Index.test.tsx
@@ -1,6 +1,7 @@
 import Home from "../pages/index";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { movieService } from "../service/movieService";
 
 global.scrollTo = jest.fn();
 global.fetch = jest.fn(
@@ -37,17 +38,36 @@ global.fetch = jest.fn(
   }
 );
 
-test("check if details is visible", () => {
-  render(<Home />);
-  expect(screen.getByTestId("movie-details")).toHaveClass("hideElement");
-});
+describe("Rendering home page components with state change", () => {
+  test("details is not visible after pageload", () => {
+    render(<Home />);
+    expect(screen.getByTestId("movie-details")).toHaveClass("hideElement");
+  });
 
-test("check if cards are rendered", async () => {
-  render(<Home />);
-  const form = screen.getByRole("form");
-  const input = screen.getByPlaceholderText("Find a movie...");
-  fireEvent.change(input, { target: { value: "hey" } });
-  fireEvent.submit(form);
-  const newElements = await screen.findAllByText("5");
-  expect(newElements).not.toBeNull();
+  test("cards are rendered after form submit", async () => {
+    render(<Home />);
+    const form = screen.getByRole("form");
+    const input = screen.getByPlaceholderText("Find a movie...");
+    fireEvent.change(input, { target: { value: "hey" } });
+    fireEvent.submit(form);
+    const newElements = await screen.findAllByText("5");
+    expect(newElements).not.toBeNull();
+  });
+
+  test("detail is visible after card click", async () => {
+    jest
+      .spyOn(movieService, "getMovieDetailsData")
+      .mockImplementation(() =>
+        Promise.resolve(movieService.getMovieDetailsDefaultState())
+      );
+    render(<Home />);
+    const form = screen.getByRole("form");
+    const input = screen.getByPlaceholderText("Find a movie...");
+    fireEvent.change(input, { target: { value: "hey" } });
+    fireEvent.submit(form);
+    const newElements = await screen.findAllByText("Test");
+    fireEvent.click(newElements[0]);
+    const detailsPage = await screen.findByTestId("movie-details");
+    setTimeout(() => expect(detailsPage).not.toHaveClass("hideElement"));
+  });
 });
