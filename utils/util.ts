acb509288d268e7d626edd711e42aee76d901623
tamasstephen <kicsitnagyon@gmail.com>
2022-06-20 07:29:54 +0200

    chore: Generate random id

--- utils/util.ts
+++ utils/util.ts
@@ -2,4 +2,8 @@ export const util = {
   escapeTitle(movieTitle: string): string {
     return movieTitle.replace(/[^a-zA-Z0-9\s]/g, "");
   },
+
+  generateId(): number {
+    return Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
+  },
 };
